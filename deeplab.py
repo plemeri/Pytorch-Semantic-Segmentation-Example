a8b35433b9ad5ba694f123e92ffc6d38a00bc6a3
swdsld <taehoon1018@postech.ac.kr>
2020-05-21 12:23:53 +0900

    Update deeplab.py

--- deeplab.py
+++ deeplab.py
@@ -4,101 +4,70 @@ import torch.nn.functional as F
 from backbone import *
 
 
-class AtrousSpatialPyramidPooling(nn.Module):
-    def __init__(self, in_channels, depth, output_stride):
-        super(AtrousSpatialPyramidPooling, self).__init__()
+class Pyramid_pooling_module(nn.Module):
+    def __init__(self, in_channels, depth):
+        super(Pyramid_pooling_module, self).__init__()
 
-        if output_stride == 8:
-            self.atrous_rates = [1, 12, 24, 36]
-        elif output_stride == 16:
-            self.atrous_rates = [1, 6, 12, 18]
-        else:
-            raise AttributeError('output stride is not valid')
+        self.pool_sizes = [1, 2, 3, 6]
 
-        self.conv_1x1 = nn.Sequential(
-            nn.Conv2d(in_channels, depth, kernel_size=(1, 1), dilation=self.atrous_rates[0]),
-            nn.BatchNorm2d(depth),
-            nn.ReLU())
-        self.conv_3x3_1 = nn.Sequential(
-            nn.Conv2d(in_channels, depth, kernel_size=(3, 3), dilation=self.atrous_rates[1], padding=self.atrous_rates[1]),
-            nn.BatchNorm2d(depth),
-            nn.ReLU())
-        self.conv_3x3_2 = nn.Sequential(
-            nn.Conv2d(in_channels, depth, kernel_size=(3, 3), dilation=self.atrous_rates[2], padding=self.atrous_rates[2]),
-            nn.BatchNorm2d(depth),
-            nn.ReLU())
-        self.conv_3x3_3 = nn.Sequential(
-            nn.Conv2d(in_channels, depth, kernel_size=(3, 3), dilation=self.atrous_rates[3], padding=self.atrous_rates[3]),
-            nn.BatchNorm2d(depth),
-            nn.ReLU())
-        self.image_pooling = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Conv2d(in_channels, depth, (1, 1), bias=False),
-            nn.BatchNorm2d(depth),
-            nn.ReLU()
-        )
-
-        self.conv_out = nn.Sequential(
-            nn.Conv2d(depth * 5, depth, kernel_size=(1, 1)),
-            nn.BatchNorm2d(depth),
-            nn.ReLU())
-
-        self.dropout = nn.Dropout(0.5)
+        self.pool1 = nn.Sequential(nn.AdaptiveAvgPool2d(self.pool_sizes[0]),
+                                   nn.Conv2d(in_channels, depth, kernel_size=(1, 1)),
+                                   nn.BatchNorm2d(depth),
+                                   nn.ReLU())
+        self.pool2 = nn.Sequential(nn.AdaptiveAvgPool2d(self.pool_sizes[1]),
+                                   nn.Conv2d(in_channels, depth, kernel_size=(1, 1)),
+                                   nn.BatchNorm2d(depth),
+                                   nn.ReLU())
+        self.pool3 = nn.Sequential(nn.AdaptiveAvgPool2d(self.pool_sizes[2]),
+                                   nn.Conv2d(in_channels, depth, kernel_size=(1, 1)),
+                                   nn.BatchNorm2d(depth),
+                                   nn.ReLU())
+        self.pool4 = nn.Sequential(nn.AdaptiveAvgPool2d(self.pool_sizes[3]),
+                                   nn.Conv2d(in_channels, depth, kernel_size=(1, 1)),
+                                   nn.BatchNorm2d(depth),
+                                   nn.ReLU())
 
     def forward(self, x):
-        feature1 = self.conv_1x1(x)
-        feature2 = self.conv_3x3_1(x)
-        feature3 = self.conv_3x3_2(x)
-        feature4 = self.conv_3x3_3(x)
-        feature5 = self.image_pooling(x)
-        feature5 = F.interpolate(feature5, feature1.size()[-2:], mode='bilinear', align_corners=True)
+        feature1 = self.pool1(x)
+        feature1 = F.interpolate(feature1, size=x.size()[-2:], mode='bilinear', align_corners=False)
 
-        out = torch.cat([feature1, feature2, feature3, feature4, feature5], dim=1)
+        feature2 = self.pool2(x)
+        feature2 = F.interpolate(feature2, size=x.size()[-2:], mode='bilinear', align_corners=False)
 
-        out = self.conv_out(out)
-        out = self.dropout(out)
-        return out
+        feature3 = self.pool3(x)
+        feature3 = F.interpolate(feature3, size=x.size()[-2:], mode='bilinear', align_corners=False)
+
+        feature4 = self.pool4(x)
+        feature4 = F.interpolate(feature4, size=x.size()[-2:], mode='bilinear', align_corners=False)
+
+        pyramid_feature = torch.cat([x, feature1, feature2, feature3, feature4], dim=1)
+
+        return pyramid_feature
 
 
-class DeepLab(nn.Module):
+class PspNet(nn.Module):
     def __init__(self, backbone, class_num, stride):
-        super(DeepLab, self).__init__()
+        super(PspNet, self).__init__()
 
-        # backbone
         self.backbone = eval(backbone)(pretrained=True, output_stride=stride)
+        self.ppm_in_channels = 2048
 
-        self.low_level_feature_in_channel = 256
-        self.aspp_in_channel = 2048
-        self.depth = 256
-        self.low_level_feature_reduction_channel = 48
-
-        self.atrous_spatial_pyramid_pooling = AtrousSpatialPyramidPooling(self.aspp_in_channel,
-                                                                          self.depth, stride)
-
-        self.conv_low_level_feature = nn.Sequential(
-            nn.Conv2d(self.low_level_feature_in_channel, self.low_level_feature_reduction_channel, kernel_size=(1, 1)),
-            nn.BatchNorm2d(self.low_level_feature_reduction_channel),
-            nn.ReLU())
-        
+        depth = self.ppm_in_channels // 4
+        self.pyramid_pooling_module = Pyramid_pooling_module(self.ppm_in_channels, depth)
         self.decoder = nn.Sequential(
-            nn.Conv2d(self.low_level_feature_reduction_channel + self.depth, self.depth, kernel_size=(3, 3), padding=(1, 1)),
-            nn.BatchNorm2d(self.depth),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Conv2d(self.depth, self.depth, kernel_size=(3, 3), padding=(1, 1)),
-            nn.BatchNorm2d(self.depth),
+            nn.Conv2d(self.ppm_in_channels + depth * 4, depth, kernel_size=(3, 3), padding=1),
+            nn.BatchNorm2d(depth),
             nn.ReLU(),
             nn.Dropout(0.1),
-            nn.Conv2d(self.depth, class_num, kernel_size=(1, 1))
+            nn.Conv2d(depth, class_num, kernel_size=(1, 1))
         )
 
-    def forward(self, input, label=None):
-        low_level_feature, out = self.backbone(input)
-        low_level_feature = self.conv_low_level_feature(low_level_feature)
+    def forward(self, input):
+        _, out = self.backbone(input)
 
-        out = self.atrous_spatial_pyramid_pooling(out)
-        out = F.interpolate(out, size=low_level_feature.size()[-2:], mode='bilinear', align_corners=True)
-        out = torch.cat((out, low_level_feature), dim=1)
+        out = self.pyramid_pooling_module(out)
         out = self.decoder(out)
-        out = F.interpolate(out, size=input.size()[-2:], mode='bilinear', align_corners=True)
+        out = F.interpolate(out, size=input.size()[-2:], mode='bilinear', align_corners=False)
+
+
         return out
